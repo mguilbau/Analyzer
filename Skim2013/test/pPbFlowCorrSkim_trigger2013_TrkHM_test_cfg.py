d202582f5c6f329fe5bc9b95fbd4c019edc89063
Wei <liwei810812@gmail.com>
2014-09-22 17:44:38 -0400

    Update skim configuration

---
+++ Skim2013/test/pPbFlowCorrSkim_trigger2013_TrkHM_test_cfg.py
@@ -0,0 +1,72 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("ANASKIM")
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('HeavyIonsAnalysis.Configuration.collisionEventSelection_cff')
+process.load('Configuration.EventContent.EventContentHeavyIons_cff')
+process.MessageLogger.cerr.FwkReport.reportEvery = 200
+
+process.source = cms.Source("PoolSource",
+   fileNames = cms.untracked.vstring(
+  '/store/hidata/HIRun2013A/PAHighPt/RECO/PromptReco-v1/000/210/634/FA4E6B7E-7366-E211-8DD0-0019B9F581C9.root'
+)
+)
+
+# =============== Other Statements =====================
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(200))
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+process.GlobalTag.globaltag = 'GR_P_V43F::All'
+
+# =============== Import Sequences =====================
+process.load('Appeltel.RpPbAnalysis.PAPileUpVertexFilter_cff')
+
+#Trigger Selection
+### Comment out for the timing being assuming running on secondary dataset with trigger bit selected already
+import HLTrigger.HLTfilters.hltHighLevel_cfi
+process.hltHM = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+process.hltHM.HLTPaths = ['HLT_*PixelTracks_Multiplicity*_v*']
+process.hltHM.andOr = cms.bool(True)
+process.hltHM.throw = cms.bool(False)
+
+process.PAprimaryVertexFilter = cms.EDFilter("VertexSelector",
+    src = cms.InputTag("offlinePrimaryVertices"),
+    cut = cms.string("!isFake && abs(z) <= 25 && position.Rho <= 2 && tracksSize >= 2"),
+    filter = cms.bool(True),   # otherwise it won't filter the events
+)
+
+process.PAcollisionEventSelection = cms.Sequence(process.hfCoincFilter *
+                                         process.PAprimaryVertexFilter *
+                                         process.NoScraping
+                                         )
+
+process.eventFilter_HM = cms.Sequence( 
+    process.hltHM *
+    process.PAcollisionEventSelection *
+    process.pileupVertexFilterCutGplus
+)
+
+process.eventFilter_HM_step = cms.Path( process.eventFilter_HM )
+
+process.load("davidlw.Skim2013.ppanalysisSkimContentFull_cff")
+process.output_HM = cms.OutputModule("PoolOutputModule",
+    outputCommands = process.analysisSkimContent.outputCommands,
+    fileName = cms.untracked.string('pPb_HM.root'),
+    SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('eventFilter_HM_step')),
+    dataset = cms.untracked.PSet(
+      dataTier = cms.untracked.string('AOD'),
+      filterName = cms.untracked.string('pPb_HM'))
+)
+
+process.output_HM_step = cms.EndPath(process.output_HM)
+
+process.schedule = cms.Schedule(
+    process.eventFilter_HM_step,
+    process.output_HM_step
+)
