695d1e6f29309babdcc25778dcbb25e23a3266a5
Wei <liwei810812@gmail.com>
2014-10-13 14:09:46 -0400

    Updated

--- FlowCorrAna/DiHadronCorrelationAnalyzer/python/epetadeco_cff.py
+++ FlowCorrAna/DiHadronCorrelationAnalyzer/python/epetadeco_cff.py
@@ -4,8 +4,8 @@ from FlowCorrAna.DiHadronCorrelationAnalyzer.epetadeco_cfi import *
 from FlowCorrAna.DiHadronCorrelationAnalyzer.hltFilter_cff import *
 
 epetadeco_ana_HI_hfp = epetadeco_ana.clone(
-#  TrgTrackCollection = cms.string('hiLowPtPixelTracks'),
-  TrgTrackCollection = cms.string('hiGeneralTracks'),
+  TrgTrackCollection = cms.string('hiGeneralAndPixelTracks'),
+#  TrgTrackCollection = cms.string('hiGeneralTracks'),
   VertexCollection = cms.string('hiSelectedVertex'),
   GenParticleCollection = cms.string('hiGenParticles'),
 
@@ -14,8 +14,13 @@ epetadeco_ana_HI_hfp = epetadeco_ana.clone(
 
   etaassmin = cms.double(3.0),
   etaassmax = cms.double(4.0),
-  pttrgmin = cms.vdouble(0.5),
+  pttrgmin = cms.vdouble(0.3),
 
+#  ptassmin = cms.vdouble(1.0),
+
+#  IsPtWeightTrg = cms.bool(False),
+
+#  EffFileName = cms.string('trkEffNew2012_HI_pixeltracks_tmp.root')
   EffFileName = cms.string('')
 )
 
@@ -111,18 +116,59 @@ epetadeco_ana_HI_hfm_cent5060 = epetadeco_ana_HI_hfm.clone(
 )
 
 epetadeco_ana_pPb_hfp = epetadeco_ana.clone(
+#  EffFileName = cms.string(''),
+
+#  etacms = cms.double(-0.47),
+#  etatrgmin = cms.double(-2.4),
+#  etatrgmax = cms.double(2.4-0.47*2),
+
+#  etacms = cms.double(0.47),
+#  etatrgmin = cms.double(-2.4+0.47*2),
+#  etatrgmax = cms.double(2.4),
+
+#   nvtxmax = cms.int32(1),
+
+#  ptassmin = cms.vdouble(0.3),
+
   etaassmin = cms.double(4.0),
   etaassmax = cms.double(5.0)
-#  etaassmin = cms.double(3.0),
-#  etaassmax = cms.double(4.0)
+#  etaassmin = cms.double(4.0),
+#  etaassmax = cms.double(5.2)
+
+#  etaassmin = cms.double(3.46-0.16),
+#  etaassmax = cms.double(4.26+0.04)
+#  etaassmin = cms.double(-4.26-0.04),
+#  etaassmax = cms.double(-3.46+0.16)
 )
 epetadeco_ana_pPb_hfm = epetadeco_ana.clone(
+#  EffFileName = cms.string(''),
+
+#  etacms = cms.double(-0.47),
+#  etatrgmin = cms.double(-2.4),
+#  etatrgmax = cms.double(2.4-0.47*2),
+
+#  etacms = cms.double(0.47),
+#  etatrgmin = cms.double(-2.4+0.47*2),
+#  etatrgmax = cms.double(2.4),
+
+#   nvtxmax = cms.int32(1),
+
+#  ptassmin = cms.vdouble(0.3),
+
+#  etaassmin = cms.double(4.0),
+#  etaassmax = cms.double(5.0)
+#  etaassmin = cms.double(-5.2),
+#  etaassmax = cms.double(-4.4+0.2)
+#  etaassmin = cms.double(4.4-0.2),
+#  etaassmax = cms.double(5.2)
+
   etaassmin = cms.double(-5.0),
   etaassmax = cms.double(-4.0)
-#  etaassmin = cms.double(-4.0),
-#  etaassmax = cms.double(-3.0)
+#  etaassmin = cms.double(-5.2),
+#  etaassmax = cms.double(-4.0)
 )
 
+epetadeco_ana_seq = cms.Sequence(epetadeco_ana_pPb_hfp*epetadeco_ana_pPb_hfm)
 epetadeco_ana_120150_seq = cms.Sequence(hltHM120150*epetadeco_ana_pPb_hfp*epetadeco_ana_pPb_hfm)
 epetadeco_ana_150185_seq = cms.Sequence(hltHM150185*epetadeco_ana_pPb_hfp*epetadeco_ana_pPb_hfm)
 epetadeco_ana_185220_seq = cms.Sequence(hltHM185220*epetadeco_ana_pPb_hfp*epetadeco_ana_pPb_hfm)
