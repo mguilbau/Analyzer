8de8592dafe990a331b79c05f0f6b05bb9884c4b
zc11 <zc11@rice.edu>
2015-03-24 14:48:08 -0500

    bug fix

--- FlowCorrAna/DiHadronCorrelationAnalyzer/python/PPPileUpVertexFilter_cff.py
+++ FlowCorrAna/DiHadronCorrelationAnalyzer/python/PPPileUpVertexFilter_cff.py
@@ -197,7 +197,7 @@ pileupVertexFilterCut_dz40_Gplus = pileupVertexFilterCutG.clone(
                                                                 dxyVeto = cms.double(0.05),
                                                                 dzTolerance = cms.double(4.0),
                                                                 )
-ileupVertexFilterCut_dz40_GplusPP = pileupVertexFilterCutG.clone(
+pileupVertexFilterCut_dz40_GplusPP = pileupVertexFilterCutG.clone(
                                                                  dzCutByNtrk = cms.vdouble(
                                                                                            999., 4.0, 4.0, 1.6, 1.0, 0.7, 0.5, 0.4, 0.3, 0.2, 0.0
                                                                                            ),
