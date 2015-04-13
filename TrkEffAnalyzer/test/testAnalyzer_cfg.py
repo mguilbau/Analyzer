eb927ff3fa6ee31365654e433b0bea7bb717981a
Wei <liwei810812@gmail.com>
2015-04-13 16:20:39 +0200

    Updated

--- TrkEffAnalyzer/test/testAnalyzer_cfg.py
+++ TrkEffAnalyzer/test/testAnalyzer_cfg.py
@@ -24,7 +24,7 @@ process.load("SimGeneral.TrackingAnalysis.trackingParticles_cfi")
 
 ### conditions
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'STARTHI53_V17::All'
+process.GlobalTag.globaltag = 'START44_V9E::All'
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(500)
@@ -53,41 +53,39 @@ process.options = cms.untracked.PSet(
 # Input source
 process.source = cms.Source("PoolSource",
    fileNames = cms.untracked.vstring(
-'root://xrootd1.cmsaf.mit.edu//store/user/vzhukova/HIJING_MB/HIJING_MB_RECO_v3/13a591fee6315e7fb1e99e6ba8e52eaa/reco_hijing_2582_1_tV4.root',
-'root://xrootd1.cmsaf.mit.edu//store/user/vzhukova/HIJING_MB/HIJING_MB_RECO_v3/13a591fee6315e7fb1e99e6ba8e52eaa/reco_hijing_1000_1_Bov.root'
-#'/store/user/vzhukova/HYDGET_PERIPH_batch7/HYDGET_PERIPH_RECO_batch7/b7d33bba7673cdb1ee6f4983c0800c79/HYDGET_PERIPH_RECO_10_1_7bq.root'
-#'root://xrootd1.cmsaf.mit.edu//store/user/vzhukova/HIJING_GEN-SIM_YUE-SHI_Minbias_2_v1/HIJING_RECO_YUE-SHI_Minbias__2_v1/b7d33bba7673cdb1ee6f4983c0800c79/hijing_reco_fix_4_2_RU0.root'
-#'root://xrootd1.cmsaf.mit.edu//store/himc/HiWinter13/Hijing_PPb502_MinimumBias/GEN-SIM-RECO/pa_STARTHI53_V25-v1/30000/64B44ED9-EF77-E211-825A-00266CF97FF4.root'
+'/store/mc/Fall11/MinBias_TuneZ2_7TeV-pythia6/GEN-SIM-RECODEBUG/NoPileUp_START42_V14B-v1/20000/00370B82-2540-E311-BB9E-00266CF258D8.root'
+#'root://xrootd1.cmsaf.mit.edu//store/user/vzhukova/HIJING_MB/HIJING_MB_RECO_v3/13a591fee6315e7fb1e99e6ba8e52eaa/reco_hijing_2582_1_tV4.root',
+#'root://xrootd1.cmsaf.mit.edu//store/user/vzhukova/HIJING_MB/HIJING_MB_RECO_v3/13a591fee6315e7fb1e99e6ba8e52eaa/reco_hijing_1000_1_Bov.root'
 )
 )
 
 # Track efficiency analyzer
 process.load("RiceHIG.TrkEffAnalyzer.trkEffAnalyzer_cff")
 process.load("RiceHIG.Skim2013.ppExtraReco_cff")
-process.trkEffAnalyzer.tracks = cms.untracked.InputTag('generalTracks')
-process.trackingParticleRecoTrackAsssociation.label_tr = cms.untracked.InputTag('generalTracks')
+process.trkEffAnalyzer.tracks = cms.untracked.InputTag('selectTracks')
+process.trackingParticleRecoTrackAsssociation.label_tr = cms.untracked.InputTag('selectTracks')
 process.trkEffAnalyzer.doAssociation = cms.untracked.bool(True) # association done inside analyzer (not from map)
 process.trkEffAnalyzer.hasSimInfo = cms.untracked.bool(True) # for MC matching
 
 # retracking
-process.initialStepSeeds.RegionFactoryPSet.RegionPSet.originRadius = 0.2
-process.detachedTripletStepSeeds.RegionFactoryPSet.RegionPSet.ptMin = 0.075
-process.detachedTripletStepSeeds.RegionFactoryPSet.RegionPSet.originRadius = 2.0 
-process.detachedTripletStepSeeds.RegionFactoryPSet.RegionPSet.originHalfLength = 15.0
-process.lowPtTripletStepSeeds.RegionFactoryPSet.RegionPSet.ptMin = 0.075
-process.mixedTripletStepSeedsA.RegionFactoryPSet.RegionPSet.ptMin = 0.1
-process.mixedTripletStepSeedsB.RegionFactoryPSet.RegionPSet.ptMin = 0.2
-process.pixelLessStepSeeds.RegionFactoryPSet.RegionPSet.ptMin = 0.2
-process.source.dropDescendantsOfDroppedBranches=cms.untracked.bool(False)
-process.source.inputCommands=cms.untracked.vstring(
-            'keep *',
-            'drop *_*generalTracks*_*_*',
-            'drop *_*offlinePrimaryVertices*_*_*'
-)
+#process.initialStepSeeds.RegionFactoryPSet.RegionPSet.originRadius = 0.2
+#process.detachedTripletStepSeeds.RegionFactoryPSet.RegionPSet.ptMin = 0.075
+#process.detachedTripletStepSeeds.RegionFactoryPSet.RegionPSet.originRadius = 2.0 
+#process.detachedTripletStepSeeds.RegionFactoryPSet.RegionPSet.originHalfLength = 15.0
+#process.lowPtTripletStepSeeds.RegionFactoryPSet.RegionPSet.ptMin = 0.075
+#process.mixedTripletStepSeedsA.RegionFactoryPSet.RegionPSet.ptMin = 0.1
+#process.mixedTripletStepSeedsB.RegionFactoryPSet.RegionPSet.ptMin = 0.2
+#process.pixelLessStepSeeds.RegionFactoryPSet.RegionPSet.ptMin = 0.2
+#process.source.dropDescendantsOfDroppedBranches=cms.untracked.bool(False)
+#process.source.inputCommands=cms.untracked.vstring(
+#            'keep *',
+#            'drop *_*generalTracks*_*_*',
+#            'drop *_*offlinePrimaryVertices*_*_*'
+#)
 
-process.rechits = cms.Sequence(process.siPixelRecHits * process.siStripMatchedRecHits)
-process.ppTrackReco = cms.Sequence(process.rechits * process.iterTracking * process.offlinePrimaryVertices)
-process.extra_reco = cms.Path(process.ppTrackReco)
+#process.rechits = cms.Sequence(process.siPixelRecHits * process.siStripMatchedRecHits)
+#process.ppTrackReco = cms.Sequence(process.rechits * process.iterTracking * process.offlinePrimaryVertices)
+#process.extra_reco = cms.Path(process.ppTrackReco)
 
 # Output definition
 process.RECODEBUGEventContent.outputCommands.extend(
