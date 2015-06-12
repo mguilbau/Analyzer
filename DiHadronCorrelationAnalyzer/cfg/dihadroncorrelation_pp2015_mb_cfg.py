08c4ca29333eb11e0b44dada6cde1fab274a36ac
Maxime <mguilbau@cern.ch>
2015-06-12 11:56:41 +0200

    cleaning + adding cfg files + CRAB3 files

--- DiHadronCorrelationAnalyzer/cfg/dihadroncorrelation_pp2015_mb_cfg.py
+++ DiHadronCorrelationAnalyzer/cfg/dihadroncorrelation_pp2015_mb_cfg.py
@@ -13,7 +13,9 @@ process.source = cms.Source("PoolSource",
 #'/store/user/davidlw/HIMinBiasUPC/PR2011_MBUCC_TRKANASKIM_official_v1/71a7d203fff2b3f389673e6fdd587ee0/hiGoodColl_1023_1_S52.root'
 #'root://xrootd.unl.edu//store/user/appeltel/HIMinBiasUPC/pixelTrackReco_devel_v0/a236e4501225ae15b3601563d612abb5/pixeltrackreco_6_1_qSR.root'
 #'file:/mnt/hadoop/cms/store/user/davidlw/PAHighPt/PA2013_FlowCorr_PromptReco_TrkHM_Gplus_Rereco_v15/702fa69daea9da948fe2472b4d29b5b5/pPb_HM_282_1_izm.root'
-'file:/mnt/hadoop/cms/store/user/davidlw/MinBias_TuneMonash13_13TeV-pythia8/RecoSkim_batch4_v1/00b3bbd68a41a1bc2b98cf5da8508e09/pPb_HM_35_1_qTo.root'
+#'file:/mnt/hadoop/cms/store/user/davidlw/MinBias_TuneMonash13_13TeV-pythia8/RecoSkim_batch4_v1/00b3bbd68a41a1bc2b98cf5da8508e09/pPb_HM_35_1_qTo.root'
+#'root://xrootd.unl.edu//store/user/davidlw/MinBias_TuneMonash13_13TeV-pythia8/RecoSkim_batch4_v1/00b3bbd68a41a1bc2b98cf5da8508e09/pPb_HM_35_1_qTo.root'
+'root://xrootd.cmsaf.mit.edu//store/user/davidlw/MinBias_TuneMonash13_13TeV-pythia8/RecoSkim_batch4_v1/00b3bbd68a41a1bc2b98cf5da8508e09/pPb_HM_35_1_qTo.root'
                 )
 #                                secondaryFileNames = cms.untracked.vstring('')
                             )
---
+++ DiHadronCorrelationAnalyzer/crab/CRAB3_test.py
@@ -0,0 +1,28 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = 'pp_Data_Ridge13TeV_test2'
+config.General.workArea = 'pp_Data_Ridge13TeV_test2'
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+#config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = '$CMSSW_BASE/src/FlowCorrAna/DiHadronCorrelationAnalyzer/cfg/dihadroncorrelation_pp2015_mb_cfg.py'
+
+config.section_("Data")
+#config.Data.inputDataset = '/MinBias_TuneMonash13_13TeV-pythia8/davidlw-RecoSkim_batch4_v1-00b3bbd68a41a1bc2b98cf5da8508e09/USER'
+config.Data.inputDataset = '/MinBias_TuneZ2_7TeV-pythia6/davidlw-Skim_v15-1709136df11f28c8f8a8a944d51c46a6/USER'
+#config.Data.userInputFiles = list(open('HMMC90.txt'))
+config.Data.inputDBS = 'phys03'
+#config.Data.primaryDataset = 'MinBias_TuneZ2star_7TeV_pythia6'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 10
+#config.Data.outLFN = '/store/user/zhchen/'
+config.Data.publication = False
+config.Data.publishDBS = 'phys03'
+config.Data.publishDataName = 'pp_Data_Ridge13TeV_test'
+
+config.section_("Site")
+config.Site.storageSite = 'T2_CH_CERN'
+config.Site.whitelist = ['T2_US_MIT']
